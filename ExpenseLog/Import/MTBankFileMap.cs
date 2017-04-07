318e28f4cf94446a5c854136217a43e69f0f44c3
Valentina Napolnova <palasataja@live.com>
2017-04-07 21:48:57 +0300

    tidy up

--- ExpenseLog/Import/MTBankFileMap.cs
+++ ExpenseLog/Import/MTBankFileMap.cs
@@ -1,9 +1,5 @@
 ﻿using CsvHelper.Configuration;
-using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ExpenseLog.Import
 {
